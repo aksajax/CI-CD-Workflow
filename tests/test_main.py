b183d8a1824299450f147abd7fce12b289b77d7d
Abhishek Singh <abhisheksinghm21s@gmail.com>
2026-09-12 13:39:03 +0530

    Fix failing test

--- tests/test_main.py
+++ tests/test_main.py
@@ -11,8 +11,9 @@ def test_home():
 
     assert response.status_code == 200
     assert response.json() == {
-    "message": "CI/CD Project is working!"
+    "message": "CI/CD Project is running!"
 }
+
 
 
 def test_health():
